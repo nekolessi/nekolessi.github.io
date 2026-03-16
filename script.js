1290deaffacc532c66498c0d4f9852caef029c83
nekolessi <227881292+nekolessi@users.noreply.github.com>
2026-03-16 12:02:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ const DEFAULT_ACTIVITY_ART =
 // Easy config: edit links here (add/remove/reorder as you want).
 // Social icons use Simple Icons only.
 const PROFILE_LINKS = [
-  { label: "Oshi Card", simpleIcon: "linktree", iconColor: "43E55E", href: "oshi.to/nekolessi" },
+  { label: "Oshi Card", simpleIcon: "premid", iconColor: "43E55E", href: "oshi.to/nekolessi" },
   { label: "Email", simpleIcon: "gmail", iconColor: "EA4335", href: "nekolessi.july916@passinbox.com", type: "email" },
   { label: "Twitch", simpleIcon: "twitch", iconColor: "9146FF", href: "twitch.tv/nekolessi" },
   { label: "Ko-fi", simpleIcon: "kofi", iconColor: "72A5F2", href: "ko-fi.com/nekolessi" },
   { label: "Spotify", simpleIcon: "spotify", iconColor: "1ED760", href: "open.spotify.com/user/md3unqsz1utqazf1rtrvdos09" },
-  { label: "Throne", simpleIcon: "buymeacoffee", iconColor: "FFDD00", href: "throne.com/nekolessi" },
+  { label: "Throne", simpleIcon: "ilovepdf", iconColor: "FFDD00", href: "throne.com/nekolessi" },
   { label: "Telegram", simpleIcon: "telegram", iconColor: "26A5E4", href: "t.me/nekolessi" }
 ];
 const PROFILE_REACTIONS = [
